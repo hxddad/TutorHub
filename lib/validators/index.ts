eaa8d4292387750c81040ca82c74a6e82e912763
Dawood Al-Janaby <dawoodaljanaby@gmail.com>
2026-03-28 21:02:03 -0400

    remove old redundant file

--- lib/validators/index.ts
+++
@@ -1,9 +0,0 @@
-// lib/validators/index.ts
-// Re-exports every validator so existing imports like:
-//   import { validateCourseInput } from "@/lib/validators"
-// continue to work after the split into domain files
-
-export * from "./courseValidator";
-export * from "./planningValidator";
-export * from "./messagingValidator";
-export * from "./assessmentValidator";
